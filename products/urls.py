a28b1626154834608d398ab2b3b2cfdf96b28610
enddl3224 <enddl3224@naver.com>
2022-08-05 14:55:20 +0900

    ADD: ProductDetailView - GET 메소드 추가

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from products.views import FirstCategoryView
+from products.views import FirstCategoryView,ProductDetailView
 
 urlpatterns = [
     path('/category', FirstCategoryView.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view()),
 ]
