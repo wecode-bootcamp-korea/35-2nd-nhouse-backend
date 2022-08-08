e36e974df623a7553e8c17603f2b0b6a565f482a
조예슬 <eungang3@naver.com>
2022-08-08 10:50:54 +0900

    ADD: utils.py

---
+++ core/utils.py
@@ -0,0 +1,28 @@
+import json
+
+import jwt
+
+from django.http  import JsonResponse
+from users.models import User
+from django.conf  import settings
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization')
+            payload      = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
+            user_id      = payload['user_id']
+            request.user = User.objects.get(id = user_id)
+
+            return func(self, request, *args, **kwargs)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({ 'message' : 'INVALID_TOKEN' }, status = 400)
+
+        except KeyError:
+            return JsonResponse({ 'message' : 'KEY_ERROR' }, status = 400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+
+    return wrapper
