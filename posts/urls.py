aa51fc51cab45d95a07196a8466bd472ec350ebc
조예슬 <eungang3@naver.com>
2022-08-09 13:33:16 +0900

    EDIT: edited views.py
    EDIT: views.py1

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from posts.views import PostListView
+from posts.views import PostListView, PostItemView
 
 urlpatterns = [
     path('/list', PostListView.as_view()),
+    path('/<int:post_id>', PostItemView.as_view()),
 ]
